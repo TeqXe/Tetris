9999134e19a134a04c13da19d0f1df88c382e371
TeqXe <1358582114@qq.com>
2018-01-11 18:10:40 +0800

    S 形状方块bug修改

--- src/tetris/Tetromino.java
+++ src/tetris/Tetromino.java
@@ -148,9 +148,9 @@ class I extends Tetromino {
 class S extends Tetromino {
 	public S() {
 		cells[0] = new Cell(1, 4, Tetris.S);
-		cells[1] = new Cell(0, 3, Tetris.S);
+		cells[1] = new Cell(1, 3, Tetris.S);
 		cells[2] = new Cell(0, 4, Tetris.S);
-		cells[3] = new Cell(1, 5, Tetris.S);
+		cells[3] = new Cell(0, 5, Tetris.S);
 		states = new State[2];
 		states[0] = new State(0, 0, 0, -1, -1, 0, -1, 1);
 		states[1] = new State(0, 0, -1, 0, 0, 1, 1, 1);
